5fc143a6f6a6e22eb6cada9a268b29dbbfbc320d
sachin <sachinorsharma@gmail.com>
2024-10-27 09:59:54 +0530

    add new method TypeintoTextBoxWithType_SetDelayOption

--- src/test/java/com/testscript/OpenAndTypeIntoTextBox.java
+++ src/test/java/com/testscript/OpenAndTypeIntoTextBox.java
@@ -4,7 +4,9 @@ import org.testng.annotations.Test;
 
 import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.BrowserType;
+
 import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Page.TypeOptions;
 import com.microsoft.playwright.Playwright;
 
 public class OpenAndTypeIntoTextBox {
@@ -31,4 +33,18 @@ public class OpenAndTypeIntoTextBox {
 		browser.close();
 
 	}
+	
+	@Test
+	public void TypeintoTextBoxWithType_SetDelayOption() throws InterruptedException {
+		Playwright playwright = Playwright.create();
+		Browser browser = playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(false));
+		Page page = browser.newPage();
+		page.navigate("https://www.google.com/");
+		page.type("#APjFqb","playwright test for automation",new TypeOptions().setDelay(500));
+		Thread.sleep(1500);
+		browser.close();
+
+	}
+
+	
 }
