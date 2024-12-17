b116cdcaf63026edea92b90daf8056dd7aa6ebd7
sachin <sachinorsharma@gmail.com>
2024-12-18 01:56:05 +0530

    add src files

---
+++ src/test/java/com/testscript/PlaywrightLocators.java
@@ -0,0 +1,18 @@
+package com.testscript;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.util.ArrayList;
+
+import org.testng.annotations.Test;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.Page.TypeOptions;
+
+public class PlaywrightLocators {
+
+}
