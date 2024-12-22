ff87c3d59e29d0b11758d1490f3d7b3c22e08824
sachin <sachinorsharma@gmail.com>
2024-12-23 01:58:29 +0530

    update file;

--- src/test/java/com/testscript/HandlingBasicAuth.java
+++ src/test/java/com/testscript/HandlingBasicAuth.java
@@ -12,8 +12,7 @@ public class HandlingBasicAuth {
  
 	@Test
 	public  void auththest() {
- 
- 
+  
  
 		Playwright playwright = Playwright.create();
 		Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
